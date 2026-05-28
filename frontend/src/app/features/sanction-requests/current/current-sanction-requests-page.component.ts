89d57e16e50788bab7006b54f9f3306be6edaf26
Ayal Ciobotaru <ayal.ciobotaru@gmail.com>
2026-05-28 16:09:36 -0400

    added option to editt sanction rrequests and re-request old ones

--- frontend/src/app/features/sanction-requests/current/current-sanction-requests-page.component.ts
+++ frontend/src/app/features/sanction-requests/current/current-sanction-requests-page.component.ts
@@ -1,17 +1,45 @@
 import { AsyncPipe, CurrencyPipe, DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { BehaviorSubject, switchMap } from 'rxjs';
 import { ChrvaApiService } from '../../../core/chrva-api.service';
+import { getHttpErrorMessage } from '../../../core/http-error';
 import { SanctionRequestPageHeaderComponent } from '../page-header/sanction-request-page-header.component';
 
 @Component({
   selector: 'app-current-sanction-requests-page',
   standalone: true,
-  imports: [AsyncPipe, CurrencyPipe, DatePipe, SanctionRequestPageHeaderComponent],
+  imports: [AsyncPipe, CurrencyPipe, DatePipe, RouterLink, SanctionRequestPageHeaderComponent],
   templateUrl: './current-sanction-requests-page.component.html',
   styleUrl: '../sanction-requests-table.scss'
 })
 export class CurrentSanctionRequestsPageComponent {
-  readonly current$ = this.api.getCurrentSanctionRequests();
+  private readonly refresh$ = new BehaviorSubject<void>(undefined);
+  readonly current$ = this.refresh$.pipe(
+    switchMap(() => this.api.getCurrentSanctionRequests())
+  );
+  actionError = '';
+  deletingId = '';
 
   constructor(private readonly api: ChrvaApiService) {}
+
+  deleteRequest(requestId: string, requestName: string): void {
+    this.actionError = '';
+
+    if (!window.confirm(`Delete the sanction request for ${requestName}?`)) {
+      return;
+    }
+
+    this.deletingId = requestId;
+    this.api.deleteSanctionRequest(requestId).subscribe({
+      next: () => {
+        this.deletingId = '';
+        this.refresh$.next();
+      },
+      error: (error: unknown) => {
+        this.deletingId = '';
+        this.actionError = getHttpErrorMessage(error, 'Unable to delete sanction request.');
+      }
+    });
+  }
 }
